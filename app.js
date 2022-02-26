2cc6f9072d5a4711a9aad0848e87524d68ffec5d
Catherine Komi <oceanbeaats@gmail.com>
2022-02-26 00:34:47 -0800

    start of the project

---
+++ app.js
@@ -0,0 +1,23 @@
+let container = document.querySelector('.container');
+let canvas = document.querySelector('canvas');
+let columnOne = document.querySelector('.one');
+let columnTwo = document.querySelector('.two');
+let columnThree = document.querySelector('.three');
+let columnFour = document.querySelector('.four');
+
+for(let i=1; i <=16; i++) {
+    let imgDiv = document.createElement('div');
+    let divImage = document.createElement('img');
+    divImage.setAttribute('src', `./images/${i}.jpeg`);
+    imgDiv.appendChild(divImage);
+
+    if(i < 5){
+        columnOne.appendChild(imgDiv);
+    } else if (i < 9) {
+        columnTwo.appendChild(imgDiv);
+    }else if (i < 13){
+        columnThree.appendChild(imgDiv);
+    }else{
+        columnFour.appendChild(imgDiv);
+    }
+}
